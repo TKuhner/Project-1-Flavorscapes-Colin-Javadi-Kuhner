3ebfa26d41a57a8e0f40b9eadbde3dc930889a75
TKuhner <trevorkuhner29@gmail.com>
2023-05-31 11:35:44 -0500

    committing api call url links

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+const searchAllCats = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
+const searchAllRegions = "https://www.themealdb.com/api/json/v1/1/list.php?a=list";
+const searchAllIngredients = "https://www.themealdb.com/api/json/v1/1/list.php?i=list";
+
+const searchName = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+const filterCategory = "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
+const filterRegion = "https://www.themealdb.com/api/json/v1/1/filter.php?a=";
+const filterIngredient = "https://www.themealdb.com/api/json/v1/1/filter.php?i=";
